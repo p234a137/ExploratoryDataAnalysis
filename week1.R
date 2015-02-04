7ab7863ab45ff00b2d09542ae7a96f8b65b33294
R2 D2 <p234a137@gmail.com>
2015-02-03 17:05:51 -0800

    first week quiz

--- week1.R
+++ week1.R
@@ -71,3 +71,107 @@ with(subset(pollution, region == "east"), plot(latitude, pm25, main = "East"))
 # Let you summarize the data (usually graphically) and highlight any broad features
 # Explore basic questions and hypotheses (and perhaps rule them out)
 # Suggest modeling strategies for the "next step"
+
+# # plotting systems
+# Base: "artist's palette" model
+# Lattice: Entire plot specified by one function; conditioning
+# ggplot2: Mixes elements of Base and Lattice
+
+# Base plotting system
+library(datasets)
+data(cars)
+with(cars, plot(speed, dist))
+
+# Lattice plot
+library(lattice)
+state <- data.frame(state.x77, region = state.region)
+xyplot(Life.Exp ~ Income | region, data = state, layout = c(4, 1))
+
+# ggplot2
+library(ggplot2)
+data(mpg)
+qplot(displ, hwy, data = mpg)
+
+
+# Base plotting system
+# simple graphics
+par(mfrow=c(1,1))
+library(datasets)
+hist(airquality$Ozone)  ## Draw a new plot
+with(airquality, plot(Wind, Ozone))
+airquality <- transform(airquality, Month = factor(Month))
+boxplot(Ozone ~ Month, airquality, xlab = "Month", ylab = "Ozone (ppb)")
+# with annotation
+with(airquality, plot(Wind, Ozone))
+title(main = "Ozone and Wind in New York City")  ## Add a title
+#
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City"))
+with(subset(airquality, Month == 5), points(Wind, Ozone, col = "blue"))
+#
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City", type = "n"))
+with(subset(airquality, Month == 5), points(Wind, Ozone, col = "blue"))
+with(subset(airquality, Month != 5), points(Wind, Ozone, col = "red"))
+legend("topright", pch = 1, col = c("blue", "red"), legend = c("May", "Other Months"))
+# with regression line
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City", pch = 20))
+model <- lm(Ozone ~ Wind, airquality)
+abline(model, lwd = 2)
+# multiple base plots
+par(mfrow = c(1, 2))
+with(airquality, {
+  plot(Wind, Ozone, main = "Ozone and Wind")
+  plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+})
+#
+par(mfrow = c(1, 3), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+with(airquality, {
+  plot(Wind, Ozone, main = "Ozone and Wind")
+  plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+  plot(Temp, Ozone, main = "Ozone and Temperature")
+  mtext("Ozone and Weather in New York City", outer = TRUE)
+})
+
+# graphics devices
+
+# screen
+par(mfrow = c(1, 1))
+library(datasets)
+with(faithful, plot(eruptions, waiting))  ## Make plot appear on screen device
+title(main = "Old Faithful Geyser data")  ## Annotate with a title
+
+# pdf file
+pdf(file = "myplot.pdf")  ## Open PDF device; create 'myplot.pdf' in my working directory
+## Create plot and send to a file (no plot appears on screen)
+with(faithful, plot(eruptions, waiting))
+title(main = "Old Faithful Geyser data")  ## Annotate plot; still nothing on screen
+dev.off()  ## Close the PDF file device
+## Now you can view the file 'myplot.pdf' on your computer
+
+# copying plots
+library(datasets)
+with(faithful, plot(eruptions, waiting))  ## Create plot on screen device
+title(main = "Old Faithful Geyser data")  ## Add a main title
+dev.copy(png, file = "geyserplot.png")  ## Copy my plot to a PNG file
+dev.off()  ## Don't forget to close the PNG device!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
